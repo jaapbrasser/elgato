8fb1136519e47e90bd05809ffba0313222de6af6
Jaap Brasser <noreply@jaapbrasser.com>
2021-02-01 07:04:56 +0100

    Added Identify call for Lights 🕯

--- elgato/elgato.psm1
+++ elgato/elgato.psm1
@@ -233,3 +233,34 @@ function Get-ElgatoKeyLightSettings {
         Invoke-RestMethod -Method Get -Uri http://$_`:9123/elgato/lights/settings
     }
 }
+
+
+
+function Invoke-IdentifyElgatoLight {
+    <#
+        .Synopsis
+        Gets information about an Elgato Key Light Air that's connected to the local network. Control Center and Stream Deck software not required.
+    
+        .Parameter Hostname
+        DNS Hostname or IP address of the target light, supports multiple objects
+    
+        .Example
+        Get the current key light air settings
+    
+        PS > Get-ElgatoKeyLight -Host 10.0.0.231
+    
+        .Example
+        Get the current key light air settings of both key light airs
+    
+        PS > Get-ElgatoKeyLight -Host 10.0.0.231, 10.0.0.232
+    #>
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory = $true)]
+            [string[]] $Hostname
+        )
+    
+        $HostName | ForEach-Object {
+            Invoke-RestMethod -Method Post -Uri http://$_`:9123/elgato/lights/indentify
+        }
+    }
